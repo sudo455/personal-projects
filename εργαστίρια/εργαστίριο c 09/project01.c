9ce3442c5912ce90b89df2fd7c4caa66cf87fbcd
sudo455 <aggelosmoraitis1@gmail.com>
2021-12-28 14:57:49 +0200

    28/12/21 lab09

--- εργαστίρια/εργαστίριο c 09/project01.c
+++ εργαστίρια/εργαστίριο c 09/project01.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(void){
-    
+    int x = 455;;
+    double y = 666.156455;
+    const char character = 'Μ';
+    char *string = "Άγγελος Μωρ";
+
+    printf("the value of %i is located in %p \n", x, &x);
+    printf("the value of %lf is located in %p \n", y, &y);
+    printf("the value of %c is located in %p \n", character, &character);
+    printf("the value of %s is located in %p \n", string, &string);
     return 0;
 }
